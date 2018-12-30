a1d91fa6b24a427c2fc001082dde0c52d9be8833
x899 <p.chheda29@gmail.com>
2018-12-30 20:21:29 +0530

    implement insertion sort in go

---
+++ go_sorting_algorithms/insertion_sort/insertion.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+// InsertionSort is based on the idea that one element from the input
+// elements is consumed in each iteration to find its correct position
+// i.e, the position to which it belongs in a sorted array.
+func InsertionSort(data []int) {
+	dataLen := len(data)
+
+	for i := 0; i < dataLen; i++ {
+		index := i
+		value := data[i]
+
+		for index > 0 && data[index-1] > value {
+			data[index] = data[index-1]
+			index--
+		}
+		data[index] = value
+	}
+}
+
+func main() {
+	data := []int{-229, 79, 46, 12, 58, 31, 34, 67, -89, 12, 67, 2}
+	fmt.Println("Original Array: ", data)
+	InsertionSort(data)
+	fmt.Println("  Sorted Array: ", data)
+}
