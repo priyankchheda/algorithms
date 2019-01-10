19c84c14fb580273047b3d9e549b48c27255671a
x899 <p.chheda29@gmail.com>
2019-01-10 12:30:53 +0530

    add doubly linked list data structure

---
+++ linkedlist/doublylinkedlist/doublylinkedlist.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Node struct {
+	data int
+	prev *Node
+	next *Node
+}
+
+type DoublyLinkedList struct {
+	lenght int
+	start  *Node
+	end    *Node
+}
+
+func (list *DoublyLinkedList) Head() *Node {
+	return list.start
+}
+
+func (list *DoublyLinkedList) Tail() *Node {
+	return list.end
+}
+
+func (list *DoublyLinkedList) Lenght() int {
+	return list.lenght
+}
+
+func (list *DoublyLinkedList) InsertStart(node *Node) {
+	list.InsertAt(node, 1)
+}
+
+func (list *DoublyLinkedList) InsertEnd(node *Node) {
+	list.InsertAt(node, list.Lenght()+1)
+}
+
+func (list *DoublyLinkedList) InsertAt(node *Node, position int) error {
+	if position > list.lenght+1 || position < 0 {
+		// validation check for position value
+		// position should be between 0 and list lenght + 1
+		return errors.New("invalid position")
+
+	} else if list.lenght == 0 {
+		// if linked list is empty
+		list.start = node
+		list.end = node
+
+	} else if position == list.lenght+1 {
+		// inserting element to last position (InsertEnd)
+		current := list.end
+		list.end = node
+		current.next = node
+		node.prev = current
+
+	} else if position == 1 {
+		// inserting element to first position (InsertStart)
+		current := list.start
+		list.start = node
+		node.next = current
+		current.prev = node
+
+	} else {
+		// inserting at nth position
+		current := list.start
+		for i := 1; i < position; i++ {
+			current = current.next
+		}
+		currentPrev := current.prev
+		node.prev = currentPrev
+		node.next = current
+		currentPrev.next = node
+		current.prev = node
+	}
+	list.lenght++
+	return nil
+}
+
+func (list *DoublyLinkedList) DeleteStart() (int, error) {
+	return list.DeleteAt(1)
+}
+
+func (list *DoublyLinkedList) DeleteEnd() (int, error) {
+	return list.DeleteAt(list.lenght)
+}
+
+func (list *DoublyLinkedList) DeleteAt(position int) (int, error) {
+	if list.lenght < position {
+		// validation check for position value
+		// position should not be more than list lenght
+		return 0, errors.New("not that much elements")
+
+	} else if list.lenght == 0 {
+		// if linked list is empty
+		return 0, errors.New("list is empty")
+
+	} else if list.lenght == 1 {
+		// if linked list only contains one element
+		current := list.start
+		list.start = nil
+		list.end = nil
+		list.lenght = 0
+		return current.data, nil
+
+	} else if position == 1 {
+		// deleting first element in the linked list (DeleteStart)
+		current := list.start
+		list.start = list.start.next
+		list.start.prev = nil
+		current.next = nil
+		list.lenght--
+		return current.data, nil
+
+	} else if position == list.lenght {
+		// deleting last element in the linked list (DeleteEnd)
+		current := list.end
+		list.end = list.end.prev
+		list.end.next = nil
+		list.lenght--
+		return current.data, nil
+
+	} else {
+		// deleting nth element
+		current := list.start
+		for i := 1; i < position; i++ {
+			current = current.next
+		}
+		current.prev.next = current.next
+		current.next.prev = current.prev
+		list.lenght--
+		return current.data, nil
+	}
+}
+
+func (list *DoublyLinkedList) PrintList() {
+	current := list.start
+	for {
+		fmt.Printf("%d", current.data)
+		if current.next == nil {
+			break
+		}
+		current = current.next
+		fmt.Printf(" -> ")
+	}
+	fmt.Println()
+}
---
+++ linkedlist/doublylinkedlist/doublylinkedlist_test.go
@@ -0,0 +1,274 @@
+package main
+
+import (
+	"testing"
+)
+
+func getDLLDataInSlice(list *DoublyLinkedList) []int {
+	var data []int
+	current := list.start
+	for {
+		if current == nil {
+			break
+		}
+		data = append(data, current.data)
+		current = current.next
+	}
+	return data
+}
+
+func TestInsertStart(t *testing.T) {
+	tables := []struct {
+		data     []int
+		expected []int
+	}{
+		{[]int{1, 2, 3, 4, 5}, []int{5, 4, 3, 2, 1}},
+		{[]int{67, 34, 23, 78, 78, 12}, []int{12, 78, 78, 23, 34, 67}},
+		{[]int{367, 834, 242}, []int{242, 834, 367}},
+	}
+
+	for _, table := range tables {
+		list := DoublyLinkedList{0, nil, nil}
+		array := table.data
+		expected_array := table.expected
+		for _, nodeData := range array {
+			list.InsertStart(&Node{nodeData, nil, nil})
+		}
+		output := getDLLDataInSlice(&list)
+		for i := 0; i < len(array); i++ {
+			if output[i] != expected_array[i] {
+				t.Errorf("Wrong Output expected: %d got: %d",
+					array[i], output[i])
+			}
+		}
+	}
+}
+
+func TestInsertEnd(t *testing.T) {
+	tables := []struct {
+		data []int
+	}{
+		{[]int{1, 2, 3, 4, 5}},
+		{[]int{67, 34, 23, 78, 78, 12}},
+		{[]int{367, 834, 242}},
+	}
+
+	for _, table := range tables {
+		list := DoublyLinkedList{0, nil, nil}
+		array := table.data
+		for _, nodeData := range array {
+			list.InsertEnd(&Node{nodeData, nil, nil})
+		}
+		output := getDLLDataInSlice(&list)
+		for i := 0; i < len(array); i++ {
+			if output[i] != array[i] {
+				t.Errorf("Wrong Output expected: %d got: %d",
+					array[i], output[i])
+			}
+		}
+	}
+}
+
+func TestHeadTailLenght(t *testing.T) {
+	tables := []struct {
+		data []int
+	}{
+		{[]int{1, 2, 3, 4, 5}},
+		{[]int{67, 34, 23, 78, 78, 12}},
+		{[]int{367, 834, 242}},
+	}
+
+	for _, table := range tables {
+		list := DoublyLinkedList{0, nil, nil}
+		array := table.data
+		for _, nodeData := range array {
+			list.InsertEnd(&Node{nodeData, nil, nil})
+		}
+		output := getDLLDataInSlice(&list)
+		head := list.Head()
+		if output[0] != head.data {
+			t.Errorf("Wrong Head expected: %d got: %d", output[0], head.data)
+		}
+
+		tail := list.Tail()
+		if output[len(output)-1] != tail.data {
+			t.Errorf("Wrong Tail expected: %d got: %d",
+				output[len(output)-1], tail.data)
+		}
+		lenght := list.Lenght()
+		if len(output) != lenght {
+			t.Errorf("Wrong lenght expected: %d got: %d", len(output), lenght)
+		}
+	}
+}
+
+func TestDeleteStart1(t *testing.T) {
+	tables := []struct {
+		data []int
+	}{
+		{[]int{67}},
+		{[]int{1, 2, 3, 4, 5}},
+		{[]int{67, 34, 23, 78, 78, 12}},
+		{[]int{367, 834, 242}},
+	}
+
+	for _, table := range tables {
+		list := DoublyLinkedList{0, nil, nil}
+		array := table.data
+		for _, nodeData := range array {
+			list.InsertEnd(&Node{nodeData, nil, nil})
+		}
+		output := getDLLDataInSlice(&list)
+		for _, nodeData := range output {
+			deletedNode, err := list.DeleteStart()
+			if err != nil {
+				t.Errorf("error msg: %v", err)
+			}
+			if deletedNode != nodeData {
+				t.Errorf("Wrong output expected: %d got:%d",
+					nodeData, deletedNode)
+			}
+		}
+	}
+}
+
+func TestDeleteStart2(t *testing.T) {
+	list := DoublyLinkedList{0, nil, nil}
+	deletedNode, err := list.DeleteStart()
+	if err == nil {
+		t.Errorf("Error: no error when deleting from empty list")
+	}
+	if deletedNode != 0 {
+		t.Errorf("deleted node is not zerod value")
+	}
+}
+
+func TestDeleteEnd1(t *testing.T) {
+	tables := []struct {
+		data []int
+	}{
+		{[]int{67}},
+		{[]int{1, 2, 3, 4, 5}},
+		{[]int{67, 34, 23, 78, 78, 12}},
+		{[]int{367, 834, 242}},
+	}
+
+	for _, table := range tables {
+		list := DoublyLinkedList{0, nil, nil}
+		array := table.data
+		for _, nodeData := range array {
+			list.InsertStart(&Node{nodeData, nil, nil})
+		}
+		output := getDLLDataInSlice(&list)
+		for i := len(output) - 1; i >= 0; i-- {
+			deletedNode, err := list.DeleteEnd()
+			if err != nil {
+				t.Errorf("Error Msg: %v", err)
+			}
+			if output[i] != deletedNode {
+				t.Errorf("Wrong Output expected: %d got: %d",
+					output[i], deletedNode)
+			}
+		}
+	}
+}
+
+func TestDeleteEnd2(t *testing.T) {
+	list := DoublyLinkedList{0, nil, nil}
+	deletedNode, err := list.DeleteEnd()
+	if err == nil {
+		t.Errorf("Error: no error when deleting from empty list")
+	}
+	if deletedNode != 0 {
+		t.Errorf("deleted node is not zerod value")
+	}
+}
+
+func TestDeleteAt1(t *testing.T) {
+	tables := []struct {
+		data        []int
+		position    int
+		output      []int
+		deletedItem int
+	}{
+		{[]int{67}, 1, []int{}, 67},
+		{[]int{1, 2, 3, 4, 5}, 3, []int{1, 2, 4, 5}, 3},
+		{[]int{67, 34, 23, 78, 78, 12}, 5, []int{67, 34, 23, 78, 12}, 78},
+		{[]int{367, 834, 242}, 3, []int{367, 834}, 242},
+		{[]int{367, 834, 242}, 1, []int{834, 242}, 367},
+	}
+	for _, table := range tables {
+		list := DoublyLinkedList{0, nil, nil}
+		for _, nodeData := range table.data {
+			list.InsertEnd(&Node{nodeData, nil, nil})
+		}
+		deletedNode, err := list.DeleteAt(table.position)
+		if err != nil {
+			t.Errorf("Error: %v", err)
+		}
+		if table.deletedItem != deletedNode {
+			t.Errorf("Wrong Output expected: %v got: %v",
+				table.deletedItem, deletedNode)
+		}
+		listAfterDeleting := getDLLDataInSlice(&list)
+		for i, nodeData := range listAfterDeleting {
+			if nodeData != table.output[i] {
+				t.Errorf("Wrong Output expected: %v, got: %v",
+					table.output, listAfterDeleting)
+			}
+		}
+	}
+
+}
+
+func TestDeleteAt2(t *testing.T) {
+	list := DoublyLinkedList{0, nil, nil}
+	deletedNode, err := list.DeleteAt(9)
+	if err == nil {
+		t.Errorf("Error: %v", err)
+	}
+	if deletedNode != 0 {
+		t.Errorf("deleted node is not zero valued")
+	}
+}
+
+func TestInsertAt(t *testing.T) {
+	tables := []struct {
+		input    []int
+		element  int
+		position int
+		output   []int
+	}{
+		{[]int{}, 45, 1, []int{45}},
+		{[]int{67}, 45, 1, []int{45, 67}},
+		{[]int{1, 2, 3, 4, 5}, 6, 6, []int{1, 2, 3, 4, 5, 6}},
+		{[]int{67, 34, 23, 78, 78, 12}, 12, 4, []int{67, 34, 23, 12, 78, 78, 12}},
+		{[]int{367, 834, 242}, 345, 2, []int{367, 345, 834, 242}},
+	}
+
+	for _, table := range tables {
+		list := DoublyLinkedList{0, nil, nil}
+		for _, nodeData := range table.input {
+			list.InsertEnd(&Node{nodeData, nil, nil})
+		}
+		err := list.InsertAt(&Node{table.element, nil, nil}, table.position)
+		if err != nil {
+			t.Errorf("Error: %v\n", err)
+		}
+		finalList := getDLLDataInSlice(&list)
+		for i := 0; i < len(finalList); i++ {
+			if finalList[i] != table.output[i] {
+				t.Errorf("Wrong output got: %v expected: %v",
+					finalList, table.output)
+			}
+		}
+	}
+}
+
+func TestInsertAt2(t *testing.T) {
+	list := DoublyLinkedList{0, nil, nil}
+	err := list.InsertAt(&Node{6, nil, nil}, 9)
+	if err == nil {
+		t.Errorf("Error: %v", err)
+	}
+}
