b81a32c7c161c60fc1d338604a0cdf112aa53227
x899 <p.chheda29@gmail.com>
2019-01-18 23:02:12 +0530

    code refactoring quick sort test cases

--- sort/sort_test.go
+++ sort/sort_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/x899/algorithms/shuffle"
 	mysort "github.com/x899/algorithms/sort"
 )
 
@@ -175,4 +176,69 @@ func TestMergeSortOptimizedRandom(t *testing.T) {
 			}
 		}
 	}
+}
+
+func testShuffleSort(t *testing.T, sortFunc func([]int, int, int)) {
+	tests := []struct {
+		original []int
+		sorted   []int
+	}{
+		{nil, nil},
+		{[]int{}, []int{}},
+		{[]int{5}, []int{5}},
+		{[]int{5, 8, 2, 9}, []int{2, 5, 8, 9}},
+		{[]int{-9, -3, 9, -12}, []int{-12, -9, -3, 9}},
+	}
+
+	for _, test := range tests {
+		dataLen := len(test.original)
+		shuffle.Shuffle(test.original)
+		sortFunc(test.original, 0, dataLen-1)
+
+		for i := 0; i < dataLen; i++ {
+			if test.original[i] != test.sorted[i] {
+				t.Errorf("wrong output expected: %v got: %v",
+					test.sorted, test.original)
+			}
+		}
+	}
+
+}
+
+func testShuffleRandom(t *testing.T, sortFunc func([]int, int, int)) {
+	for i := 0; i < 5; i++ {
+		random := rand.New(rand.NewSource(time.Now().UnixNano()))
+		array1 := make([]int, random.Intn(10000))
+		for i := range array1 {
+			array1[i] = random.Intn(10000)
+		}
+		array2 := make([]int, len(array1))
+		copy(array2, array1)
+
+		shuffle.Shuffle(array1)
+		sortFunc(array1, 0, len(array1)-1)
+		sort.Ints(array2)
+
+		for i := 0; i < len(array1); i++ {
+			if array1[i] != array2[i] {
+				t.Errorf("wrong output expected: %v got: %v",
+					array2, array1)
+			}
+		}
+	}
+}
+
+func TestQuickSort(t *testing.T) {
+	testShuffleSort(t, mysort.QuickSort)
+	testShuffleRandom(t, mysort.QuickSort)
+}
+
+func TestQuickSortOptimized(t *testing.T) {
+	testShuffleSort(t, mysort.QuickSortOptimized)
+	testShuffleRandom(t, mysort.QuickSortOptimized)
+}
+
+func TestQuick3WaySort(t *testing.T) {
+	testShuffleSort(t, mysort.Quick3WaySort)
+	testShuffleRandom(t, mysort.Quick3WaySort)
 }
