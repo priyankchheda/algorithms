41897327f44effad6900ed9ae3801add51087ce5
x899 <p.chheda29@gmail.com>
2019-01-20 23:08:42 +0530

    queue code refactoring

--- queue/queue.go
+++ queue/queue.go
@@ -1,19 +1,25 @@
+// Package queue is a implementation of queue data structure in go
 package main
 
 import "errors"
 
+// Queue struct contains data and methods are queue operations.
 type Queue struct {
 	array []int
 }
 
+// NewQueue creates new empty integer queue.
 func NewQueue() Queue {
 	return Queue{array: make([]int, 0)}
 }
 
+// Enqueue adds an item to the queue.
 func (q *Queue) Enqueue(value int) {
 	q.array = append(q.array, value)
 }
 
+// Dequeue removes an item front the queue.
+// Returns error if the queue is empty.
 func (q *Queue) Dequeue() (removedElement int, err error) {
 	if q.IsEmpty() {
 		return 0, errors.New("queue is empty")
@@ -24,10 +30,12 @@ func (q *Queue) Dequeue() (removedElement int, err error) {
 	return removedElement, nil
 }
 
+// Size returns the number of elements currently present in queue
 func (q *Queue) Size() int {
 	return len(q.array)
 }
 
+// IsEmpty checks if the queue is empty or not
 func (q *Queue) IsEmpty() bool {
 	return len(q.array) <= 0
 }
--- queue/queue_test.go
+++ queue/queue_test.go
@@ -4,35 +4,7 @@ import (
 	"testing"
 )
 
-func TestEnqueue(t *testing.T) {
-	tables := []struct {
-		data []int
-	}{
-		{[]int{67}},
-		{[]int{1, 2, 3, 4, 5}},
-		{[]int{67, 34, 23, 78, 78, 12}},
-		{[]int{367, 834, 242}},
-	}
-
-	for _, table := range tables {
-		q := NewQueue()
-		for i, element := range table.data {
-			q.Enqueue(element)
-			if i+1 != q.Size() {
-				t.Errorf("Incorrect Size expected: %d got: %d",
-					i+1, q.Size())
-			}
-		}
-		for i := 0; i < len(table.data); i++ {
-			if q.array[i] != table.data[i] {
-				t.Errorf("Wrong Output expected: %d got: %d",
-					table.data[i], q.array[i])
-			}
-		}
-	}
-}
-
-func TestDequeue1(t *testing.T) {
+func TestEnqueueDequeue(t *testing.T) {
 	tables := []struct {
 		data []int
 	}{
@@ -65,7 +37,7 @@ func TestDequeue1(t *testing.T) {
 	}
 }
 
-func TestDequeue2(t *testing.T) {
+func TestDequeueEmpty(t *testing.T) {
 	q := NewQueue()
 	removedItem, err := q.Dequeue()
 	if err == nil {
