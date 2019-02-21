b280e52983954ca0be47e1dcb54f0254ac57d3e3
x899 <p.chheda29@gmail.com>
2019-02-21 17:02:43 +0530

    binary search documentation

--- search/binarysearch.go
+++ search/binarysearch.go
@@ -1,5 +1,16 @@
+// Package search is an implementation of various search algorithm in GoLang
 package search
 
+// BinarySearch is the most efficient searching algorithm having a run-time
+// complexity of O(log2 N). This algorithm works only on a sorted list of
+// elements.
+//
+// Binary search begins by comparing the middle element of the list with the
+// target element. If the target value matches the middle element, its position
+// in the list is returned. If it does not match, the list is divided into two
+// halves. The first half consists of the first element to middle element
+// whereas the second half consists of the element next to the middle element
+// to the last element.
 func BinarySearch(arr []int, value int) int {
 	start := 0
 	end := len(arr) - 1
