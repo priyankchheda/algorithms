1f0f09ed9e48c8ddcc328c68086c4e7ede6a3d4e
Suvid Sahay <42059321+suvidsahay@users.noreply.github.com>
2019-10-27 17:11:06 +0530

    Update oddeven.go

--- sort/oddeven.go
+++ sort/oddeven.go
@@ -6,13 +6,13 @@ func OddEvenSort(data []int){
 
 	for sorted == false{
 		sorted = true
-		for i := 1; i < dataLen-1; i++ {
+		for i := 1; i < dataLen-1; i+=2 {
 			if data[i] > data[i+1] {
 				data[i], data[i+1] = data[i+1], data[i]
 				sorted = false
 			}
 		}
-		for i := 0; i < dataLen-1; i++ {
+		for i := 0; i < dataLen-1; i+=2 {
 			if data[i] > data[i+1] {
 				data[i], data[i+1] = data[i+1], data[i]
 				sorted = false
