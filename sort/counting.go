f65a9f9957893fa4695f82ffca6a39534d0a0ed0
suvidsahay <sahay.suvid61@gmail.com>
2019-10-28 22:55:32 +0530

    few updates

---
+++ sort/counting.go
@@ -0,0 +1,9 @@
+package sort
+
+func countingsort(data []int){
+	max := data[0];
+	min := data[0];
+	for i,e := range data{
+		if()
+	}
+}
--- sort/oddeven.go
+++ sort/oddeven.go
@@ -1,22 +1,31 @@
 package sort
 
-func OddEvenSort(data []int){
+//Odd-Even Sort is a variation of bubble sort where the sorting is divided into 
+//two phases, Odd and Even Phase and it runs until all the elements are sorted
+//In the odd phase we perform bubble sort on odd indexed elements and in the 
+//even phase we perform bubble sort on even indexed elements.
+
+func OddEvenSort(data []int) {
 	sorted := false
 	dataLen := len(data)
 
-	for sorted == false{
+	for {
 		sorted = true
-		for i := 1; i < dataLen-1; i++ {
+		for i := 1; i < dataLen-1; i += 2 {
 			if data[i] > data[i+1] {
 				data[i], data[i+1] = data[i+1], data[i]
 				sorted = false
 			}
 		}
-		for i := 0; i < dataLen-1; i++ {
+		for i := 0; i < dataLen-1; i += 2 {
 			if data[i] > data[i+1] {
 				data[i], data[i+1] = data[i+1], data[i]
 				sorted = false
 			}
+		}
+		if sorted {
+			break		
 		}
 	}
 }
+
