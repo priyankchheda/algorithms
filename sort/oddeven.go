95912d2fa588d2b3d567a9c0440aa81ac7dbc220
suvidsahay <sahay.suvid61@gmail.com>
2019-10-31 11:15:36 +0530

    Odd Even Sort Implementation

--- sort/oddeven.go
+++ sort/oddeven.go
@@ -1,8 +1,20 @@
 package sort
 
+<<<<<<< HEAD
+<<<<<<< HEAD
 //Odd-Even Sort is a variation of bubble sort where the sorting is divided into
 //two phases, Odd and Even Phase and it runs until all the elements are sorted
 //In the odd phase we perform bubble sort on odd indexed elements and in the
+=======
+//Odd-Even Sort is a variation of bubble sort where the sorting is divided into 
+//two phases, Odd and Even Phase and it runs until all the elements are sorted
+//In the odd phase we perform bubble sort on odd indexed elements and in the 
+>>>>>>> f65a9f9... few updates
+=======
+//Odd-Even Sort is a variation of bubble sort where the sorting is divided into
+//two phases, Odd and Even Phase and it runs until all the elements are sorted
+//In the odd phase we perform bubble sort on odd indexed elements and in the
+>>>>>>> 2d960f3... Deleted half implemented counting sort and formatted Odd-Even Sort
 //even phase we perform bubble sort on even indexed elements.
 
 func OddEvenSort(data []int) {
@@ -32,7 +44,15 @@ func OddEvenSort(data []int) {
 			}
 		}
 		if sorted {
+<<<<<<< HEAD
+<<<<<<< HEAD
 			break
+=======
+			break		
+>>>>>>> f65a9f9... few updates
+=======
+			break
+>>>>>>> 2d960f3... Deleted half implemented counting sort and formatted Odd-Even Sort
 		}
 	}
 }
