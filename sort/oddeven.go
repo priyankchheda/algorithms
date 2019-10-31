525e4535dc7cde06032b6ee3bac7cf5fd4f6d0d6
Suvid Sahay <42059321+suvidsahay@users.noreply.github.com>
2019-10-31 11:41:37 +0530

    Update in Odd Even Sort

--- sort/oddeven.go
+++ sort/oddeven.go
@@ -1,20 +1,8 @@
 package sort
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 //Odd-Even Sort is a variation of bubble sort where the sorting is divided into
 //two phases, Odd and Even Phase and it runs until all the elements are sorted
 //In the odd phase we perform bubble sort on odd indexed elements and in the
-=======
-//Odd-Even Sort is a variation of bubble sort where the sorting is divided into 
-//two phases, Odd and Even Phase and it runs until all the elements are sorted
-//In the odd phase we perform bubble sort on odd indexed elements and in the 
->>>>>>> f65a9f9... few updates
-=======
-//Odd-Even Sort is a variation of bubble sort where the sorting is divided into
-//two phases, Odd and Even Phase and it runs until all the elements are sorted
-//In the odd phase we perform bubble sort on odd indexed elements and in the
->>>>>>> 2d960f3... Deleted half implemented counting sort and formatted Odd-Even Sort
 //even phase we perform bubble sort on even indexed elements.
 
 func OddEvenSort(data []int) {
@@ -23,36 +11,20 @@ func OddEvenSort(data []int) {
 
 	for {
 		sorted = true
-<<<<<<< HEAD
 		for i := 1; i < dataLen-1; i += 2 {
-=======
-		for i := 1; i < dataLen-1; i+=2 {
->>>>>>> 1f0f09e... Update oddeven.go
 			if data[i] > data[i+1] {
 				data[i], data[i+1] = data[i+1], data[i]
 				sorted = false
 			}
 		}
-<<<<<<< HEAD
 		for i := 0; i < dataLen-1; i += 2 {
-=======
-		for i := 0; i < dataLen-1; i+=2 {
->>>>>>> 1f0f09e... Update oddeven.go
 			if data[i] > data[i+1] {
 				data[i], data[i+1] = data[i+1], data[i]
 				sorted = false
 			}
 		}
 		if sorted {
-<<<<<<< HEAD
-<<<<<<< HEAD
 			break
-=======
-			break		
->>>>>>> f65a9f9... few updates
-=======
-			break
->>>>>>> 2d960f3... Deleted half implemented counting sort and formatted Odd-Even Sort
 		}
 	}
 }
