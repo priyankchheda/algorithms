d336e716e43bf7a82721121eacbcb16731b742f8
x899 <p.chheda29@gmail.com>
2019-12-05 11:37:01 +0530

    added set test cases for remove, pop, clear, isempty

--- set/set_test.go
+++ set/set_test.go
@@ -54,3 +54,98 @@ func TestAddMultiple(t *testing.T) {
 		t.Error("added items are not availabile in the set.")
 	}
 }
+
+func TestRemoveSingle(t *testing.T) {
+	s := set.NewSet()
+	s.Add(1)
+	s.Add(2)
+	s.Add(3)
+
+	s.Remove(2)
+	if s.Size() != 2 {
+		t.Error("Remove: set size should be two after removing")
+	}
+
+	s.Remove(2)
+	if s.Size() != 2 {
+		t.Error("Remove: set size should not change after removing 2 again")
+	}
+
+	s.Remove(1)
+	s.Remove(3)
+	if s.Size() != 0 {
+		t.Error("Remove: set size should be zero")
+	}
+
+	// try to remove something from a zero length set
+	s.Remove(34)
+}
+
+func TestRemoveMultiple(t *testing.T) {
+	s := set.NewSet()
+	s.Add(1, 3, 4, 5)
+	s.Remove()
+	if s.Size() != 4 {
+		t.Error("Remove: items are not unique. The set size should be four")
+	}
+
+	s.Remove(3, 4, 5)
+	if s.Size() != 1 {
+		t.Error("Remove: items are not unique. The set size should be one")
+	}
+
+	if !s.Has(1) {
+		t.Error("added items are not available in the set.")
+	}
+}
+
+func TestPop(t *testing.T) {
+	s := set.NewSet()
+	s.Add(1, 3, 4, 5)
+	a, _ := s.Pop()
+	if s.Size() != 3 {
+		t.Error("Pop: set size should be two after popping out")
+	}
+
+	if s.Has(a) {
+		t.Error("Pop: returned item should not exist")
+	}
+
+	s.Pop()
+	s.Pop()
+	s.Pop()
+	b, err := s.Pop()
+	if b != 0 {
+		t.Error("Pop: should return 0 because set is empty")
+	}
+	if err.Error() != "set is empty" {
+		t.Error("error message should be 'set is empty'")
+	}
+
+	//try to remove something from a zero length set
+	s.Pop()
+}
+
+func TestClear(t *testing.T) {
+	s := set.NewSet()
+	s.Add(1)
+	s.Add(2)
+	s.Add(3, 4, 5)
+
+	s.Clear()
+	if s.Size() != 0 {
+		t.Error("Clear: set size should be zero")
+	}
+}
+
+func TestIsEmpty(t *testing.T) {
+	s := set.NewSet()
+	if !s.IsEmpty() {
+		t.Error("IsEmpty: set is empty, it should be true")
+	}
+
+	s.Add(1, 2, 3)
+	if s.IsEmpty() {
+		t.Error("IsEmpty: set is filled, it should be false")
+	}
+}
