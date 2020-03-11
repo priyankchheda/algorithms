8b1d1d5ad846b21cc7dc3936c458843947f6316a
priyankchheda <p.chheda29@gmail.com>
2020-03-11 19:06:38 +0530

    add linked list docstrings

--- LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include "LinkedList.hpp"
 
+/**
+ * Returns the number of node currently present in the linked list.
+ * @return count of node, or 0 if the list is empty.
+*/
 int LinkedList::getLength() {
     Node* temp = head;
     int count = 0;
@@ -11,17 +15,31 @@ int LinkedList::getLength() {
     return count;
 }
 
+/**
+ * Returns the number of node currently present in the linked list.
+ * It uses recursive approach.
+ * @param node starting node from where counting is start.
+ * @return count of node, or 0 if the list is empty.
+*/
 int LinkedList::getLengthRecursive(Node* node) {
     if (node == nullptr) return 0;
     return 1 + getLengthRecursive(node->next);
 }
 
+/**
+ * Inserts new node with value 'data' at the start of linked list.
+ * @param data new data element that needs to be added at the start
+*/
 void LinkedList::insertHead(int data) {
     Node* temp = new Node(data);
     temp->next = head;
     head = temp;
 }
 
+/**
+ * Inserts new node with value 'data' at the end of linked list.
+ * @param data new data element that needs to be append at the end
+*/
 void LinkedList::insertEnd(int data) {
     Node* newNode = new Node(data);
     Node* temp = head;
@@ -35,6 +53,12 @@ void LinkedList::insertEnd(int data) {
     temp->next = newNode;
 }
 
+/**
+ * Insert new node with value 'data' at given position in linked list.
+ * @param data new data element that needs to be added at given position.
+ * @param position position where new node will enter.
+ *      Position is 1-based.
+*/
 void LinkedList::insertAt(int data, int position) {
     int linkedlist_len = getLength();
     if (position <= 1) {
@@ -56,6 +80,10 @@ void LinkedList::insertAt(int data, int position) {
     }
 }
 
+/**
+ * Delete head node from linked list
+ * @return removedData value of deleted node
+*/
 int LinkedList::deleteHead() {
     if (head == nullptr) return -1;
     Node* temp = head;
@@ -65,6 +93,10 @@ int LinkedList::deleteHead() {
     return removedData;
 }
 
+/**
+ * Delete end node from linked list
+ * @return removedData value of deleted node
+*/
 int LinkedList::deleteEnd() {
     if (head == nullptr) return -1;
     Node* current = head;
@@ -82,6 +114,12 @@ int LinkedList::deleteEnd() {
     return removedData;
 }
 
+/**
+ * Delete node present at given position from linked list
+ * @param position position at which node needs to be deleted.
+ *      Position is 1-based.
+ * @return removedData value of deleted node
+*/
 int LinkedList::deleteAt(int position) {
     int ll_length = getLength();
     if (position < 1 || position > ll_length)
@@ -106,10 +144,19 @@ int LinkedList::deleteAt(int position) {
     }
 }
 
+/**
+ * Delete first occurance of node from linked list with value 'key'
+ * @param key node value that needs to be deleted
+ * @return removedData value of deleted node
+*/
 int LinkedList::deleteKey(int key) {
     return deleteAt(indexOf(key));
 }
 
+/**
+ * Deletes entire linked list. Cleans up the memory.
+ * head node pointer is pointing to null.
+*/
 void LinkedList::deleteList() {
     Node* current = head;
     Node* next = nullptr;
@@ -121,6 +168,11 @@ void LinkedList::deleteList() {
     head = nullptr;
 }
 
+/**
+ * Get node value present at given position in the linked list
+ * @param position position at which node value is needed. Position is 1-based.
+ * @return node value at position
+*/
 int LinkedList::dataAt(int position) {
     int ll_length = getLength();
     if (position < 1 || position > ll_length)
@@ -133,6 +185,11 @@ int LinkedList::dataAt(int position) {
     return current->data;
 }
 
+/**
+ * Returns index of node in linked list with node value 'key'.
+ * Returns -1, if the key is not present.
+ * @param count index of node with value 'key'
+*/
 int LinkedList::indexOf(int key) {
     Node* node = head;
     int count = 1;
@@ -145,6 +202,9 @@ int LinkedList::indexOf(int key) {
     return -1;
 }
 
+/**
+ * Reverse the original linked list using iterative approach.
+*/
 void LinkedList::Reverse() {
     Node* previous = nullptr;
     Node* following = nullptr;
@@ -159,6 +219,9 @@ void LinkedList::Reverse() {
     head = previous;
 }
 
+/**
+ * Reverse the original linked list using recursive approach.
+*/
 void LinkedList::ReverseRecursive(Node* node) {
     if (node == nullptr)
         return;
@@ -172,6 +235,9 @@ void LinkedList::ReverseRecursive(Node* node) {
     node->next = nullptr;
 }
 
+/**
+ * printList simply prints the node value from head to end.
+*/
 void LinkedList::printList() {
     Node* temp = head;
     while (temp) {
@@ -181,12 +247,20 @@ void LinkedList::printList() {
     std::cout << std::endl;
 }
 
+/**
+ * printListRecursive simply prints the node value from head to end.
+ * It uses recursive approach.
+*/
 void LinkedList::printListRecursive(Node* node) {
     if (node == nullptr) return;
     std::cout << " -> " << node->data;
     printListRecursive(node->next);
 }
 
+/**
+ * printReverseListRecursive simply prints the node value in reverse order,
+ * from tail to head. It uses recursive approach.
+*/
 void LinkedList::printReverseListRecursive(Node* node) {
     if (node == nullptr) return;
     printReverseListRecursive(node->next);
