ea29d7e2b473df2f566c4525860dc1d757d4da7b
priyankchheda <p.chheda29@gmail.com>
2020-04-03 10:27:28 +0530

    add stack balance paranthesis

---
+++ Stack/balancedParanthesis.cpp
@@ -0,0 +1,53 @@
+/**
+ * Check for balanced parentheses in an expression
+ * CMD: g++ -std=c++14 -Wall balancedParanthesis.cpp
+ */
+
+#include <iostream>
+#include "Stack/Stack.hpp"
+
+bool areParanthesisBalanced(std::string);
+
+/* main driver function */
+int main()
+{
+    std::string exp = "{a*(a+b)} + [4+5]";
+    bool result = areParanthesisBalanced(exp);
+    if (result) std::cout << "balanced\n";
+    else std::cout << "not balanced\n";
+    return 0;
+}
+
+/**
+ * check if the paranthesis in given expression exp is balanced or not.
+ * @param exp expression to be checked.
+ * @returns true if balance, else false
+*/
+bool areParanthesisBalanced(std::string exp)
+{
+    Stack::Stack<char> stack;
+    int expLen = exp.length();
+
+    for (int i = 0; i < expLen; i++) {
+        // if character is any of the opening braces, push it to stack
+        if (exp[i] == '(' || exp[i] == '{' || exp[i] == '[') {
+            stack.push(exp[i]);
+
+        // if character is any of the closing braces and peek of stack is
+        // corresponding opening braces, pop the opening brace from stack
+        // ignore every other character
+        } else if (exp[i] == ')' || exp[i] == '}' || exp[i] == ']') {
+            if ((exp[i] == ')' && stack.peek() == '(') ||
+                (exp[i] == '}' && stack.peek() == '{') ||
+                (exp[i] == ']' && stack.peek() == '['))
+                stack.pop();
+            else return false;
+        }
+    }
+
+    // after parsing all the characters in exp, expression is unbalanced
+    // if stack is not empty.
+    if (!stack.isEmpty())
+        return false;
+    return true;
+}
